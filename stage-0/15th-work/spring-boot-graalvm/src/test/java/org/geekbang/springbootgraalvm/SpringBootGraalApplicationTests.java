249186f0d88f0d8f2294f9db3d2f64ad17b04555
ben <419822241@qq.com>
2021-06-16 14:58:06 +0800

    test pass

---
+++ stage-0/15th-work/spring-boot-graalvm/src/test/java/org/geekbang/springbootgraalvm/SpringBootGraalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.geekbang.springbootgraalvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGraalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
